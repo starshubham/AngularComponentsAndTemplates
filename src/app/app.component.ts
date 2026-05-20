36c1b26ebdff4d4015dfc5e021dc4c226e8a6963
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-05-20 18:45:30 +0530

    Content Projection with multiple slots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { TrafficComponent } from "./dashboard/traffic/traffic.component";
-import { TicketComponent } from "./dashboard/ticket/ticket.component";
+import { TicketsComponent } from "./dashboard/tickets/tickets.component";
 import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
@@ -13,7 +13,7 @@ import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-ite
     HeaderComponent, 
     ServerStatusComponent, 
     TrafficComponent, 
-    TicketComponent, 
+    TicketsComponent, 
     DashboardItemComponent
   ],
 })
