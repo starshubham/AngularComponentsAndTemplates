5fa21ec23b007ede447f48606cc140c7658932b7
SHUBHAM SETH <shubham.seth@greenpaynetwork.co.in>
2026-06-03 08:58:43 +0530

    Template Variables

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // onSubmit(titleElement: HTMLInputElement) {
+  //   // console.dir(titleElement);
+  //   const enteredTitle = titleElement.value;
+  //   console.log('Entered Title:', enteredTitle);
+  // }
 
+  onSubmit(title: string, ticketText: string) {
+    console.log(title);
+    console.log(ticketText);
+  }
 }
